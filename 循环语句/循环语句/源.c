e3642e29691f2a06e65161e478c6bb9962b03588
s2w1z <112538010+s2w1z@users.noreply.github.com>
2022-09-01 23:19:27 +0800

    Add files via upload

---
+++ 循环语句/循环语句/源.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	while ( line < 10000)
+	{
+		printf("ÎÒ°®Äã\n");
+		line++;
+	 }
+	if (line == 10000)
+	{
+		printf("½á»é\n");
+
+	}
+	return 0;
+}
